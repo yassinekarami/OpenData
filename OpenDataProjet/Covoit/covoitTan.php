465a696db91e080417bfbcc2944920578a0f80eb
yassinekarami <yassine.karami@outlook.com>
2018-10-29 17:22:32 +0000

    final

---
+++ OpenDataProjet/Covoit/covoitTan.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../Css/w3.css">
+    <link rel="stylesheet" href="../Css/font.css">
+    <link rel="stylesheet" href="../Css/styleAccueil.css" />
+    <link rel="stylesheet" href="../Css/styleButton.css">
+    <title>Projet</title>
+
+<body>
+
+<br>
+<!-- Sidebar/menu -->
+
+<nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
+
+    <a href="../index.php" ><h1>ACCUEIL</h1></a>
+    <div class="w3-bar-block">
+
+        <table>
+            <tr>
+                <td> <button style="background-color: white ; color: black;"><a href="covoitIndex.php"  class="w3-bar-item w3-button w3-hover-white">Tan</a></button></td>
+            </tr>
+            <tr>
+                <td> <button ><a href="covoitVelo.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Bicloo</a></button></td>
+            </tr>
+            <tr>
+                <td> <button><a href="covoitMarguerite.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Station marguerite</a></button></td>
+            </tr>
+
+            <tr>
+                <td> <button style="background-color: white ; color: black;"><a href="../Tan/tanIndex.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Aire de covoiturage</a></button></td>
+            </tr>
+        </table>
+    </div>
+
+</nav>
+
+<header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
+
+    <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right" onclick="w3_open()">☰</a>
+
+</header>
+
+
+
+<div class="w3-main" style="margin-left:400px;margin-right:40px">
+
+    <div class="w3-container" style="margin-top:20px" id="showcase">
+
+        <img src="../image/covoitCircuits.png">
+
+    </div>
+
+    <div class="gallery">
+
+
+    </div>
+
+</body>
+</head>
+</html>
